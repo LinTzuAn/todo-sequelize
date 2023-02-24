36cb9c33febe993c787d90ae59f66b4fa021b4f5
LinTzuAn <a22341254@gmail.com>
2023-02-24 14:43:34 +0800

    feat: add home route and page

--- app.js
+++ app.js
@@ -5,19 +5,26 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine','hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => { return res.render('index', { todos }) })
+    .catch(error => { return res.status(422).json(error) })
 })
 
 app.get('/users/login', (req, res) => {
@@ -33,7 +40,9 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password})
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
