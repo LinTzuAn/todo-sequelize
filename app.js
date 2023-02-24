905cb3f8b9ae5b61d904c332368aed1ee5c20eed
LinTzuAn <a22341254@gmail.com>
2023-02-24 18:48:30 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const PORT = 3000
 
 const routes = require('./routes')
 
-usePassport(app)
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine','hbs')
 
@@ -22,6 +20,14 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 app.use(routes)
 
